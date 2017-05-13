f4a61fe29970edc4faff3970eb7c2fd18d6d2f65
jatindera <schoolstepsin@gmail.com>
2017-05-13 14:09:00 +0530

    MQTT - Controlling LED module using Mobile App

--- ESP8266/MQTT-MobileAPP_LED/mqttconfig.lua
+++ ESP8266/MQTT-MobileAPP_LED/mqttconfig.lua
@@ -1,4 +1,6 @@
 HOST = "m13.cloudmqtt.com"
 PORT = 14940
-CLIENTID1 = "client100"
-ENDPOINT_LED = "MyLED"
+CLIENTID1 = "client1"
+CLIENTID2 = "client2"
+ENDPOINT_MOTION = "motiondetected"
+ENDPOINT_LED = "ledstate"
